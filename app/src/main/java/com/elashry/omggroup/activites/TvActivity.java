9752bd7fdffba333de815adbd209df9d3e996d9e
Emadmagdy151995Fci <emadmagdi.151995@gmai.com>
2020-02-16 22:39:26 +0200

    emad

--- app/src/main/java/com/elashry/omggroup/activites/TvActivity.java
+++ app/src/main/java/com/elashry/omggroup/activites/TvActivity.java
@@ -44,42 +44,41 @@ import retrofit2.Retrofit;
 
 public class TvActivity extends AppCompatActivity {
 
-    private String url, bg,hashtag = "";
-    private Boolean ads,active_logo,active_image_ads,active_time,active_hash_tag;
+    private String url, bg, hashtag = "";
+    private Boolean ads, active_logo, active_image_ads, active_time, active_hash_tag;
     private VideoView videoViewAds;//,videoView
     private VideoView videoView;
 
     private FrameLayout flAd;
     private RelativeLayout app_video_box;
-    private ImageView image, imgOrientation,imgLogo,imgads;
-    private ProgressBar progBar, progBarLoad,progBarLoad2;
-    private TextView tvCounter,tvHashtag;
-    private MediaPlayer mp,mp2;
-    private CountDownTimer adsTime, timer,imgTime;
+    private ImageView image, imgOrientation, imgLogo, imgads;
+    private ProgressBar progBar, progBarLoad, progBarLoad2;
+    private TextView tvCounter, tvHashtag;
+    private MediaPlayer mp, mp2;
+    private CountDownTimer adsTime, timer, imgTime;
     private MediaController mediaController;
     private boolean isNormal = true;
-    private AdView adView,adView2;
+    private AdView adView, adView2;
 
-    private ProgressBar progBarAd,progBarAd2;
-    private TextView tvClock,tvAd,tvAd2,tvTitleAd,tvTitleAd2;
-    private ImageView imageAd,imageAd2;
+    private ProgressBar progBarAd, progBarAd2;
+    private TextView tvClock, tvAd, tvAd2, tvTitleAd, tvTitleAd2;
+    private ImageView imageAd, imageAd2;
     private Preference preference;
     private FrameLayout flAd1;
-    private AdsDataModel.ItemModel itemModel=null;
-    private ImageView imgDelete1,imgDelete2;
+    private AdsDataModel.ItemModel itemModel = null;
+    private ImageView imgDelete1, imgDelete2;
     private RewardedVideoAd mRewardedVideoAd;
     private final String VIDEO = "http://admin.omgchannel.net/storage/";
     private List<VideoAdsModel.Video> videoList;
-    private int period =20,periodImg=20;
-    private int currentVideoIndex = 0,currentImgIndex=0;
-    private boolean isTimerStarted = false,isImgTimerStarted=false;
+    private int period = 20, periodImg = 20;
+    private int currentVideoIndex = 0, currentImgIndex = 0;
+    private boolean isTimerStarted = false, isImgTimerStarted = false;
     private LinearLayout Lin_imgAds;
     private List<String> imageAdsModelList;
     private View view;
-    private int current_pos =0;
+    private int current_pos = 0;
     private boolean adsRunning = false;
     private boolean isFirstTime = true;
-
 
 
     @Override
@@ -98,12 +97,12 @@ public class TvActivity extends AppCompatActivity {
         if (intent != null) {
             url = intent.getStringExtra("url");
             bg = intent.getStringExtra("bg");
-            ads=intent.getBooleanExtra("ads",false);
-            active_hash_tag=intent.getBooleanExtra("active_hash_tag",false);
-            active_image_ads=intent.getBooleanExtra("active_image_ads",false);
-            active_logo=intent.getBooleanExtra("active_logo",false);
-            active_time=intent.getBooleanExtra("active_time",false);
-            hashtag=intent.getStringExtra("hashtag");
+            ads = intent.getBooleanExtra("ads", false);
+            active_hash_tag = intent.getBooleanExtra("active_hash_tag", false);
+            active_image_ads = intent.getBooleanExtra("active_image_ads", false);
+            active_logo = intent.getBooleanExtra("active_logo", false);
+            active_time = intent.getBooleanExtra("active_time", false);
+            hashtag = intent.getStringExtra("hashtag");
 
         }
     }
@@ -127,7 +126,6 @@ public class TvActivity extends AppCompatActivity {
         progBarLoad = findViewById(R.id.progBarLoad);
         progBarLoad.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.blue), PorterDuff.Mode.SRC_IN);
 
-
         tvCounter = findViewById(R.id.tvCounter);
 
         videoView = findViewById(R.id.videoView);
@@ -136,24 +134,22 @@ public class TvActivity extends AppCompatActivity {
         progBarLoad2.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.blue), PorterDuff.Mode.SRC_IN);
 
         videoViewAds = findViewById(R.id.videoViewAds);
-        tvClock=findViewById(R.id.tvClock);
+        tvClock = findViewById(R.id.tvClock);
 
-        Lin_imgAds=findViewById(R.id.Lin_imgAds);
-        imgads=findViewById(R.id.imgads);
-        tvHashtag=findViewById(R.id.tvHashtag);
+        Lin_imgAds = findViewById(R.id.Lin_imgAds);
+        imgads = findViewById(R.id.imgads);
+        tvHashtag = findViewById(R.id.tvHashtag);
         tvHashtag.setText(hashtag);
 
-        if (!active_image_ads)
-        {
+        if (!active_image_ads) {
             Lin_imgAds.setVisibility(View.GONE);
-        }else {
+        } else {
             Lin_imgAds.setVisibility(View.VISIBLE);
 
         }
-        if (!active_hash_tag)
-        {
+        if (!active_hash_tag) {
             tvHashtag.setVisibility(View.GONE);
-        }else {
+        } else {
             tvHashtag.setVisibility(View.VISIBLE);
 
         }
@@ -161,33 +157,30 @@ public class TvActivity extends AppCompatActivity {
 
         image.setImageResource(R.drawable.bg);
 
-        imgLogo=findViewById(R.id.imgLogo);
+        imgLogo = findViewById(R.id.imgLogo);
 
         Glide.with(TvActivity.this)
-                .load(R.drawable.logo_gif)
+                .load(R.drawable.logo_omg)
                 .into(imgLogo);
 
         progBarAd = findViewById(R.id.progBarAd);
 
         tvAd = findViewById(R.id.tvAd);
 
-        if (!ads){
+        if (!ads) {
             flAd1.setVisibility(View.GONE);
+        } else {
+            getAds();
         }
-        else {
-        getAds();
-        }
-        if (!active_time)
-        {
-         tvClock.setVisibility(View.GONE);
-        }else {
+        if (!active_time) {
+            tvClock.setVisibility(View.GONE);
+        } else {
             tvClock.setVisibility(View.VISIBLE);
 
         }
-        if (!active_logo)
-        {
+        if (!active_logo) {
             imgLogo.setVisibility(View.GONE);
-        }else {
+        } else {
             imgLogo.setVisibility(View.VISIBLE);
 
         }
@@ -196,11 +189,9 @@ public class TvActivity extends AppCompatActivity {
         getVideoAds();
 
 
-
-
     }
-    private void getData()
-    {
+
+    private void getData() {
         Retrofit retrofit = Api.getClient("http://admin.omgchannel.net/");
         Services services = retrofit.create(Services.class);
         Call<List<ImageAdsModel>> call = services.getImgAds();
@@ -208,15 +199,13 @@ public class TvActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<ImageAdsModel>> call, Response<List<ImageAdsModel>> response) {
 
-                if (response.isSuccessful()&&response.body()!=null&&response.body().size()>0) {
+                if (response.isSuccessful() && response.body() != null && response.body().size() > 0) {
 
-                    if (response.body().get(0).getImages().size()>0)
-                    {
+                    if (response.body().get(0).getImages().size() > 0) {
                         periodImg = response.body().get(0).getPeriod();
                         imageAdsModelList.addAll(response.body().get(0).getImages());
-                        if (response.body().get(0).getImages().size()>1)
-                        {
-                            Picasso.with(TvActivity.this).load(Uri.parse(VIDEO+imageAdsModelList.get(currentImgIndex))).fit().into(imgads);
+                        if (response.body().get(0).getImages().size() > 1) {
+                            Picasso.with(TvActivity.this).load(Uri.parse(VIDEO + imageAdsModelList.get(currentImgIndex))).fit().into(imgads);
 
                             currentImgIndex++;
                             startTimerAds(response.body().get(0).getPeriod());
@@ -238,25 +227,21 @@ public class TvActivity extends AppCompatActivity {
             }
         });
     }
-    private void getAds()
-    {
+
+    private void getAds() {
         String id = preference.getUserId(this);
         Retrofit retrofit = Api.getClient("https://recommendation.speakol.com/");
         Services services = retrofit.create(Services.class);
-        Call<AdsDataModel> call = services.getAdsData("wi-4071","fa8dbbcb682699544e4e8f2212115f73",id);
+        Call<AdsDataModel> call = services.getAdsData("wi-4071", "fa8dbbcb682699544e4e8f2212115f73", id);
         call.enqueue(new Callback<AdsDataModel>() {
             @Override
             public void onResponse(Call<AdsDataModel> call, Response<AdsDataModel> response) {
 
-                if (response.isSuccessful()&&response.body()!=null) {
+                if (response.isSuccessful() && response.body() != null) {
 
-                    if (response.body().getPayload()!=null&&response.body().getPayload().getItems()!=null)
-                    {
-                        if (response.body().getPayload().getItems().get(0)!=null)
-                        {
+                    if (response.body().getPayload() != null && response.body().getPayload().getItems() != null) {
+                        if (response.body().getPayload().getItems().get(0) != null) {
                             itemModel = response.body().getPayload().getItems().get(0);
-
-
 
 
                         }
@@ -275,8 +260,8 @@ public class TvActivity extends AppCompatActivity {
             }
         });
     }
-    private void getVideoAds()
-    {
+
+    private void getVideoAds() {
 
         Retrofit retrofit = Api.getClient("http://admin.omgchannel.net/");
         Services services = retrofit.create(Services.class);
@@ -284,7 +269,7 @@ public class TvActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<VideoAdsModel>>() {
             @Override
             public void onResponse(Call<List<VideoAdsModel>> call, Response<List<VideoAdsModel>> response) {
-                if (response.isSuccessful()&&response.body()!=null) {
+                if (response.isSuccessful() && response.body() != null) {
 
 
                     period = response.body().get(0).getPeriod();
@@ -295,25 +280,23 @@ public class TvActivity extends AppCompatActivity {
                     playVideo();
 
 
-
-
                 } else {
 
                     playVideo();
-                    Toast.makeText(TvActivity.this,"فشل", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TvActivity.this, "فشل", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<List<VideoAdsModel>> call, Throwable t) {
                 playVideo();
-                Log.e("error",t.getMessage()+"__");
+                Log.e("error", t.getMessage() + "__");
                 Toast.makeText(TvActivity.this, "تحقق من الاتصال بالانترنت", Toast.LENGTH_LONG).show();
             }
         });
     }
-    private void playVideo()
-    {
+
+    private void playVideo() {
 
         view.setVisibility(View.GONE);
         videoView.setVideoPath(url);
@@ -321,145 +304,87 @@ public class TvActivity extends AppCompatActivity {
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                Log.e("rrr","rrrr");
-
-                Log.e("adds",adsRunning+"_");
-                Log.e("isFirst",isFirstTime+"_");
 
                 progBarLoad.setVisibility(View.GONE);
 
-                if (adsRunning)
-                {
+                if (adsRunning) {
 
 
                     videoView.start();
 
-                }else
-                    {
+                } else {
 
-                        if (isFirstTime)
-                        {
+                    if (isFirstTime) {
 
-                            videoView.start();
+                        videoView.start();
 
-                            if (videoList.size()>0)
-                            {
+                        if (videoList.size() > 0) {
 
-                                startTimer(period);
-                            }
+                            startTimer(period);
+                        }
 
-                        }else
-                            {
-                                Log.e("bb","bb");
-                                videoView.start();
-                                if (videoList.size()>0)
-                                {
-                                    startTimer(period);
+                    } else {
+                        Log.e("bb", "bb");
+                        videoView.start();
+                        if (videoList.size() > 0) {
+                            startTimer(period);
 
-                                }
-                            }
-
-
-
+                        }
                     }
 
 
-
+                }
 
 
             }
         });
 
-        /*videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                progBarLoad.setVisibility(View.VISIBLE);
-                if (timer!=null)
-                {
-                    timer.cancel();
 
-                }
-                playVideo();
-            }
-        });*/
 
 
     }
+
     private void playVideoAds() {
 
-        //progBarLoad.setVisibility(View.VISIBLE);
         current_pos = videoView.getCurrentPosition();
 
-        String urlVideo = VIDEO+videoList.get(currentVideoIndex).getDownloadLink();
-       /* videoViewAds.setVideoPath(urlVideo);
-        videoViewAds.requestFocus();*/
+        String urlVideo = VIDEO + videoList.get(currentVideoIndex).getDownloadLink();
 
-       videoView.setVideoPath(urlVideo);
-       videoView.requestFocus();
+        videoView.setVideoPath(urlVideo);
+        videoView.requestFocus();
 
-
-       /*//video ads
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                videoView.start();
-
-               *//* progBarLoad2.setVisibility(View.GONE);
-                videoViewAds.setAlpha(1.0f);
-                videoView.pause();
-                current_pos = videoView.getCurrentPosition();
-
-
-                videoViewAds.start();*//*
-
-            }
-        });
-*/
-
-        //vid ads
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(final MediaPlayer mp) {
 
-                Log.e("complete","complete");
+                Log.e("complete", "complete");
 
-                if (adsRunning)
-                {
+                if (adsRunning) {
 
                     currentVideoIndex++;
 
-                    if (currentVideoIndex<videoList.size())
-                    {
+                    if (currentVideoIndex < videoList.size()) {
                         progBarLoad.setVisibility(View.VISIBLE);
                         view.setVisibility(View.GONE);
-                        String urlVideo = VIDEO+videoList.get(currentVideoIndex).getDownloadLink();
+                        String urlVideo = VIDEO + videoList.get(currentVideoIndex).getDownloadLink();
                         videoView.setVideoPath(urlVideo);
 
-                        //videoViewAds.setVideoURI(Uri.parse(urlVideo));
-                    }else
-                    {
+                    } else {
 
 
                         isFirstTime = false;
                         adsRunning = false;
-                        // videoViewAds.setVisibility(View.GONE);
                         currentVideoIndex = 0;
-                        //progBarLoad2.setVisibility(View.GONE);
                         view.setVisibility(View.GONE);
                         progBarLoad.setVisibility(View.VISIBLE);
                         videoView.setVideoPath(url);
 
 
-
                     }
-                }else
-                    {
-                        isFirstTime = true;
-                        playVideo();
-                    }
-
-
-
+                } else {
+                    isFirstTime = true;
+                    playVideo();
+                }
 
 
             }
@@ -467,9 +392,9 @@ public class TvActivity extends AppCompatActivity {
     }
 
     private void startTimer(int time) {
-        isTimerStarted =true;
+        isTimerStarted = true;
 
-        timer = new CountDownTimer(1000*time, 1000) {
+        timer = new CountDownTimer(1000 * time, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
@@ -477,8 +402,7 @@ public class TvActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-               /* progBarLoad2.setVisibility(View.VISIBLE);
-                videoViewAds.setVisibility(View.VISIBLE);*/
+
                 progBarLoad.setVisibility(View.VISIBLE);
                 isTimerStarted = false;
                 isFirstTime = false;
@@ -486,10 +410,6 @@ public class TvActivity extends AppCompatActivity {
                 adsRunning = true;
 
                 playVideoAds();
-
-
-
-
 
 
             }
@@ -500,8 +420,8 @@ public class TvActivity extends AppCompatActivity {
     }
 
     private void startTimerAds(int time) {
-        isImgTimerStarted =true;
-        imgTime = new CountDownTimer(1000*time, 1000) {
+        isImgTimerStarted = true;
+        imgTime = new CountDownTimer(1000 * time, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
@@ -513,15 +433,14 @@ public class TvActivity extends AppCompatActivity {
                 isImgTimerStarted = false;
 
 
-                if (currentImgIndex<imageAdsModelList.size())
-                {
-                    Picasso.with(TvActivity.this).load(Uri.parse(VIDEO+imageAdsModelList.get(currentImgIndex))).fit().into(imgads);
+                if (currentImgIndex < imageAdsModelList.size()) {
+                    Picasso.with(TvActivity.this).load(Uri.parse(VIDEO + imageAdsModelList.get(currentImgIndex))).fit().into(imgads);
                     imgTime.start();
                     currentImgIndex++;
-                }else {
+                } else {
 
-                    currentImgIndex= 0;
-                    Picasso.with(TvActivity.this).load(Uri.parse(VIDEO+imageAdsModelList.get(currentImgIndex))).fit().into(imgads);
+                    currentImgIndex = 0;
+                    Picasso.with(TvActivity.this).load(Uri.parse(VIDEO + imageAdsModelList.get(currentImgIndex))).fit().into(imgads);
                     imgTime.start();
                 }
 
@@ -545,7 +464,6 @@ public class TvActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onPause() {
         super.onPause();
@@ -562,18 +480,16 @@ public class TvActivity extends AppCompatActivity {
         videoView.setVisibility(View.VISIBLE);
         view.setVisibility(View.GONE);
         videoView.resume();
-        if (videoList.size()>0)
-        {
-            if (!isTimerStarted){
+        if (videoList.size() > 0) {
+            if (!isTimerStarted) {
                 startTimer(period);
 
             }
 
         }
 
-        if (imageAdsModelList.size()>0)
-        {
-            if (!isImgTimerStarted){
+        if (imageAdsModelList.size() > 0) {
+            if (!isImgTimerStarted) {
                 startTimerAds(periodImg);
 
             }
@@ -581,7 +497,6 @@ public class TvActivity extends AppCompatActivity {
         }
 
     }
-
 
 
     @Override
@@ -606,8 +521,6 @@ public class TvActivity extends AppCompatActivity {
     public void onBackPressed() {
         finish();
     }
-
-
 
 
 }
